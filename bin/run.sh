125dcd3271df33846eddf78a9d4d83d4e74037b5
cschen <chungshiuan@gmail.com>
2015-01-28 14:06:48 +0800

    repair run.sh and build.sbt

--- bin/run.sh
+++ bin/run.sh
@@ -2,16 +2,18 @@
 FWDIR="$(cd `dirname $0`/..; pwd)"
 BUILDPATH=$FWDIR/"build.sbt"
 echo $BUILDPATH
-PROJECTNAME=`grep name $BUILDPATH | awk -F := '{print $2}'`
-PROJECTVERSION=`grep version $BUILDPATH | awk -F := '{print $2}'`
-SCALAVERSION=`grep scalaVersion $BUILDPATH | awk -F := '{print $2}'`
+PROJECTNAME=`grep name $BUILDPATH | awk -F := '{print $2}' | sed 's/[[:blank:]]//g' | sed 's/"//g'`
+PROJECTVERSION=`grep version $BUILDPATH | awk -F := '{print $2}' | sed 's/[[:blank:]]//g' | sed 's/"//g'`
+SCALAVERSION=`grep scalaVersion $BUILDPATH | awk -F := '{print $2}' | sed 's/[[:blank:]]//g' | sed 's/"//g' | awk -F \. '{print $1"."$2}'`
 echo "---->"$PROJECTNAME
 echo "---->"$PROJECTVERSION
 echo "---->"$SCALAVERSION
-export JAR_HOME=$FWDIR/lib
-JARPATH=`ls ../lib/ |sed "s:^:$JAR_HOME\/:"|xargs | sed  's/[[:blank:]]/:/g'`
-echo "?????"$PROJECTNAME\_$SCALAVERSION-$PROJECTVERSION
-JARPATH=$JARPATH:$FWDIR/target/scala-$SCALAVERSION/$PROJECTNAME\_$SCALAVERSION-$PROJECTVERSION.jar
+JAR_HOME=$FWDIR/lib
+JARPATH="`ls $JAR_HOME |sed "s:^:$JAR_HOME\/:"| xargs | sed  's/[[:blank:]]/:/g'`"
+echo $JARPATH
+JARPATH=$JARPATH":"$FWDIR"/target/scala-"$SCALAVERSION"/"$PROJECTNAME"_"$SCALAVERSION"-"$PROJECTVERSION.jar
 echo "JARPATH-->"$JARPATH
 JARPATH=$JARPATH":$SCALA_HOME/lib/scala-library.jar"
 java -cp "$JARPATH" "$@"
+#$PROJECTNAME=`grep name $BUILDPATH | awk -F := '{print $2}'`
+
--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ name := "schat"
 
 version := "0.0.0"
 
-scalaVersion :="2.11"
+scalaVersion :="2.11.1"
 
 libraryDependencies ++= {
        val slf4jVersion = "1.7.5"
